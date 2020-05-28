b4c69e2896f736790897c93445632831aff5e658
DNKO <denis.krainovic@outlook.at>
2020-05-28 20:05:31 +0200

    add client

---
+++ client/src/CommentList.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default ({comments}) => {
+    const renderComments = comments.map(comment => {
+        let content;
+
+        if (comment.status === "approved"){
+            content = comment.content
+        } else if (comment.status === "rejected"){
+            content = "This comment has been rejected."
+        } else if (comment.status === "pending"){
+            content = "This comment is awaiting moderation."
+        }
+        
+        return <li key={comment.id}>{content}</li>
+    })
+
+    return <div>
+        {renderComments}
+    </div>
+}
